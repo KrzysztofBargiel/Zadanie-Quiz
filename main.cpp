43790dc28c38f46daa88460ed7966a35d8be677f
KrzysztofBargiel <zychu02@gmail.com>
2014-04-28 16:38:12 +0200

    Test

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "Pytania.h"
+
+using namespace std;
+
+int main()
+{
+    Pytanie p[5];
+    int suma = 0;
+    
+    for (int i = 0; i <=4; i++)
+    {
+        p[i].nr_pytania = i+1;
+        p[i].wczytaj();
+        p[i].zadaj();
+        p[i].sprawdz();
+        suma += p[i].punkt;
+    }
+    
+    
+    cout<<"Koniec, punkty = : " << suma << endl;
+  
+    return 0;
+}
+
